edb3314ca7a775d640cd875c56c7c3ca6591d4ee
Ewen Auffret <auffret.ewen@gmail.com>
2017-08-05 20:09:23 +0200

    Added an UI to change the block materials

---
+++ Assets/Resources/Scripts/UIControler.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIControler : MonoBehaviour
+{
+
+    public BrushState state;
+    public GameObject toolBar;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!RectTransformUtility.RectangleContainsScreenPoint(toolBar.GetComponent<RectTransform>(),
+                             Input.mousePosition))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                changeBlock();
+            }
+
+            else if (Input.GetMouseButtonDown(1))
+            {
+                this.state = BrushState.NoState;
+            }
+        }
+    }
+
+    void OnMouseDown()
+    {
+
+    } 
+
+    public void setBrushState(string state)
+    {
+        this.state = (BrushState)Enum.Parse(typeof(BrushState), state, true);
+    }
+
+    private void changeBlock()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            GameObject blockHit = hit.transform.gameObject;
+            changeMaterial(blockHit);
+        }
+    }
+
+    private void changeMaterial(GameObject block)
+    {
+        switch(this.state)
+        {
+            case BrushState.Empty:
+                block.GetComponent<Renderer>().material = new Material(Shader.Find("Diffuse"));
+                break;
+            case BrushState.Grass:
+                block.GetComponent<Renderer>().material = (Material)Resources.Load("Materials/Grass");
+                break;
+            case BrushState.Mountain:
+                block.GetComponent<Renderer>().material = (Material)Resources.Load("Materials/Mountain");
+                break;
+        }
+    }
+}
