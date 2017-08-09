7e3ef446f7966c2ead9acbcda77fd98825b039ab
z3t4 <jerome.sernin@gmail.com>
2017-08-09 23:09:31 +0200

    New Project warning if map exist

---
+++ Assets/Resources/Scripts/UI/PopUp/NewProjectErrorPopUp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewProjectErrorPopUp : MonoBehaviour {
+
+    public string mapName;
+    public int x, y, playerCount;
+
+    public void onOk()
+    {
+        Project.newProject(mapName, x, y, playerCount, true);
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Resources/Scripts/UI/PopUp/NewProjectPopUp.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NewProjectPopUp : MonoBehaviour {
+
+    public NewProjectErrorPopUp errorPopUp;
+
+    public InputField nameField;
+    public InputField x;
+    public InputField y;
+    public InputField playerCount;
+    
+    public void onOk()
+    {
+        int x1, y1, playerCount1;
+        if (nameField.text.Trim() == "" ||  // Name
+            !int.TryParse(x.text, out x1) || // X
+            !int.TryParse(y.text, out y1) || // Y
+            !int.TryParse(playerCount.text, out playerCount1)) // Player count
+        {
+            return;
+        }
+        if(!Project.newProject(nameField.text, x1, y1, playerCount1, false))
+        {
+            gameObject.SetActive(false);
+            errorPopUp.x = x1;
+            errorPopUp.y = y1;
+            errorPopUp.mapName = nameField.text;
+            errorPopUp.playerCount = playerCount1;
+            errorPopUp.gameObject.SetActive(true);
+
+        }
+        UIControler.actualPopUp.SetActive(false);
+        UIControler.actualPopUp = null;
+    }
+
+    public void onCancel()
+    {
+        gameObject.SetActive(false);
+    }
+}
